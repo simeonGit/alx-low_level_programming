2a42f8bac5f36cda7a9b787bb5425ef1a21c698a
SimeonGit <kodunkopoke@gmail.com>
2022-12-15 21:19:38 -0800

    100redo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - print all posiible combinations of two different digit
  *
  * Return: 0
@@ -8,6 +7,7 @@
 int main(void)
 {
 	int digit1, digit2;
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
