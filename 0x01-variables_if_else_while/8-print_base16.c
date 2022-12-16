4668a8f04c9b9efc9305c59307e274ccd0866681
SimeonGit <kodunkopoke@gmail.com>
2022-12-15 21:12:38 -0800

    redo8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase, followed by a new line
+ * main - Prints all the numbers of base 16 in lowercase
  *
  * Return: 0
  */
