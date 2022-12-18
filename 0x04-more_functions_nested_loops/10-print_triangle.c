6df94a3b825b47f155664e9ab3ac48667d5f0655
SimeonGit <kodunkopoke@gmail.com>
2022-12-18 15:01:51 -0800

    redo

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,8 +2,11 @@
 
 /**
  * print_triangle - prints a triangle, followed by a new line
- * size: size of the triangle
+ * Description: Prints diagonals
+ * @size: size of the triangle
+ * Return: void
  */
+
 void print_triangle(int size)
 {
 	int row, hashes, spaces;
