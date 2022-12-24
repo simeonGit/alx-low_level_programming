a1d28cf0202cbd4d8bad2d55294aa987c2ab5958
SimeonGit <kodunkopoke@gmail.com>
2022-12-24 12:49:14 -0800

    2redo

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,10 +8,11 @@
  * @n: The length of int
  *
  * Return: pointer to the resulting string dest
- * */
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && *(src + i); i++)
 	{
 		*(dest + i) = *(src + i);
