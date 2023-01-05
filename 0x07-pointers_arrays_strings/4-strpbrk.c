0b388b3a2490e02727fdb9e2c0ae03d5a8c54f5a
SimeonGit <kodunkopoke@gmail.com>
2023-01-04 23:04:22 -0800

    4redo

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,14 +12,14 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
-		while (*s)
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
 		{
-			for (i = 0; accept[i]; i++)
-			{
-				if (*s == accept[i])
-					return (s);
-			}
-			s++;
+			if (*s == accept[i])
+				return (s);
 		}
-		return (NULL);
+		s++;
+	}
+	return (NULL);
 }
